abd57400d4f3b42d849924ef8c7273bc4dc7b8c8
SDlearner-maker <swetadas39@gmail.com>
2024-01-08 00:35:04 +0800

    added an empty spring project

---
+++ tryaround/tryaround/src/main/java/com/example/tryaround/TryaroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.tryaround;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryaroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryaroundApplication.class, args);
+	}
+
+}
---
+++ tryaround/tryaround/src/test/java/com/example/tryaround/TryaroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tryaround;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryaroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
